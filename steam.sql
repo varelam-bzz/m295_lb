4f146c34ed6ee07997d58a33656ba394aecec12a
varelam-bzz <varelam@bzz.ch>
2024-05-23 11:08:23 +0200

    Add sql script

---
+++ steam.sql
@@ -0,0 +1,50 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema steam
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema steam
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `steam` DEFAULT CHARACTER SET utf8 ;
+USE `steam` ;
+
+-- -----------------------------------------------------
+-- Table `steam`.`Publisher`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `steam`.`Publisher` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `Name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `steam`.`Game`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `steam`.`Game` (
+  `ID` INT NOT NULL,
+  `Name` VARCHAR(45) NOT NULL,
+  `Release_Date` DATE NOT NULL,
+  `Price` DECIMAL(10,2) NOT NULL,
+  `Purchases` INT NOT NULL,
+  `Released` TINYINT NOT NULL,
+  `PublisherID` INT NOT NULL,
+  PRIMARY KEY (`ID`),
+  INDEX `Publisher_idx` (`PublisherID` ASC) VISIBLE,
+  CONSTRAINT `PublisherID`
+    FOREIGN KEY (`PublisherID`)
+    REFERENCES `steam`.`Publisher` (`ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
