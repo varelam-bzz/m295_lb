78e6abadf99d42d6d973175d219f9b2029b8e4e6
varelam-bzz <varelam@bzz.ch>
2024-05-23 15:37:54 +0200

    Add response for ConstraintViolationExceptionHandler

--- src/main/java/ch/matias/m295_lb/utils/exceptions/ConstraintViolationExceptionHandler.java
+++ src/main/java/ch/matias/m295_lb/utils/exceptions/ConstraintViolationExceptionHandler.java
@@ -5,11 +5,16 @@ import jakarta.ws.rs.BadRequestException;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
 import jakarta.ws.rs.ext.Provider;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @Provider
 public class ConstraintViolationExceptionHandler implements ExceptionMapper<ConstraintViolationException> {
+    private final Logger logger = LogManager.getLogger(ConstraintViolationExceptionHandler.class);
+
     @Override
     public Response toResponse(ConstraintViolationException exception) {
-        throw new BadRequestException(exception.getMessage());
+        logger.error(exception.getMessage());
+        return Response.status(Response.Status.BAD_REQUEST).entity(exception.getMessage()).build();
     }
 }
