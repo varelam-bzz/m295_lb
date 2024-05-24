60d0b9b90e6a310d93aaccbcfe856700cc10271d
varelam-bzz <varelam@bzz.ch>
2024-05-24 08:05:23 +0200

    Version 1 of tests

--- src/test/java/ch/matias/m295_lb/JerseyApiTest.java
+++ src/test/java/ch/matias/m295_lb/JerseyApiTest.java
@@ -6,11 +6,10 @@ import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.*;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.junit.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.http.HttpHeaders;
 
 import java.io.IOException;
@@ -18,16 +17,17 @@ import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(JUnit4.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class JerseyApiTest {
 	private static final String SERVICE_URL
 			= "http://localhost:8080/artifact/resources/games";
 
-	private static final String USERNAME = "admin";
+	private static final String USERNAME_ADMIN = "admin";
+	private static final String USERNAME_CLEANER = "cleaner";
 	private static final String PASSWORD = "1234";
 
-	private void addAuthorizationHeader(HttpUriRequest request) {
-		String auth = STR."\{USERNAME}:\{PASSWORD}";
+	private void addAuthorizationHeader(HttpUriRequest request, String username) {
+		String auth = STR."\{username}:\{PASSWORD}";
 		byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(StandardCharsets.ISO_8859_1));
 		String authHeader = STR."Basic \{new String(encodedAuth)}";
 		request.setHeader(HttpHeaders.AUTHORIZATION, authHeader);
@@ -46,7 +46,7 @@ public class JerseyApiTest {
 	}
 
 	public void makePost(HttpPost request, String json, int status) throws IOException {
-		addAuthorizationHeader(request);
+		addAuthorizationHeader(request, USERNAME_ADMIN);
 		StringEntity entity = new StringEntity(json);
 		request.setEntity(entity);
 		request.setHeader("Content-Type", "application/json");
@@ -55,41 +55,12 @@ public class JerseyApiTest {
 	}
 
 	public void makePut(HttpPut request, String json, int status) throws IOException {
-		addAuthorizationHeader(request);
+		addAuthorizationHeader(request, USERNAME_ADMIN);
 		StringEntity entity = new StringEntity(json);
 		request.setEntity(entity);
 		request.setHeader("Content-Type", "application/json");
 
 		makeAssert(request, status);
-	}
-
-	private void initializeTestData() throws IOException {
-		HttpPost request = new HttpPost(SERVICE_URL);
-		String json = "{\"name\":\"Sekiro\",\"releaseDate\":\"2019-03-22\",\"price\":60,\"purchases\":-1000,\"released\":true,\"publisher\":{\"name\":\"FromSoftware\"}}";
-		makePost(request, json, HttpStatus.SC_OK);
-
-		json = "{\"name\":\"ExistingGame\",\"releaseDate\":\"2019-03-22\",\"price\":60,\"purchases\":-1000,\"released\":true,\"publisher\":{\"name\":\"FromSoftware\"}}";
-		makePost(request, json, HttpStatus.SC_OK);
-	}
-
-	private void cleanUpTestData() throws IOException {
-		HttpUriRequest request = new HttpDelete(SERVICE_URL);
-		addAuthorizationHeader(request);
-		HttpClientBuilder
-				.create()
-				.build()
-				.execute(request);
-	}
-
-	@BeforeEach
-	public void setUp() throws IOException {
-		cleanUpTestData();
-		initializeTestData();
-	}
-
-	@AfterEach
-	public void tearDown() throws IOException {
-		cleanUpTestData();
 	}
 
 	@Test
@@ -149,6 +120,7 @@ public class JerseyApiTest {
 	}
 
 	@Test
+	@Order(2)
 	public void getGameByName_thenOk() throws IOException {
 		HttpUriRequest request = new HttpGet(STR."\{SERVICE_URL}/byName/Sekiro");
 		HttpResponse httpResponse = HttpClientBuilder
@@ -205,6 +177,7 @@ public class JerseyApiTest {
 	}
 
 	@Test
+	@Order(1)
 	public void insertGame_thenOk() throws IOException {
 		HttpPost request = new HttpPost(SERVICE_URL);
 		String json = "{\"name\":\"Sekiro\",\"releaseDate\":\"2019-03-22\",\"price\":60,\"purchases\":1300000,\"released\":true,\"publisher\":{\"name\":\"FromSoftware\"}}";
@@ -214,35 +187,63 @@ public class JerseyApiTest {
 	@Test
 	public void insertGame_thenConflict() throws IOException {
 		HttpPost request = new HttpPost(SERVICE_URL);
-		String json = "{\"name\":\"ExistingGame\",\"releaseDate\":\"2019-03-22\",\"price\":60,\"purchases\":1300000,\"released\":true,\"publisher\":{\"name\":\"FromSoftware\"}}";
+		String json = "{\"name\":\"Sekiro\",\"releaseDate\":\"2019-03-22\",\"price\":60,\"purchases\":1300000,\"released\":true,\"publisher\":{\"name\":\"FromSoftware\"}}";
 		makePost(request, json, HttpStatus.SC_CONFLICT);
 	}
 
 	@Test
-	public void insertGame_thenBadRequest() throws IOException {
+	public void insertGameWithInvalidName_thenBadRequest() throws IOException {
 		HttpPost request = new HttpPost(SERVICE_URL);
-		String json = "{\"badkey\":\"BadGame\"}";
+		String json = "{\"name\":\"ExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGameExistingGame\",\"releaseDate\":\"2019-03-22\",\"price\":60,\"purchases\":1300000,\"released\":true,\"publisher\":{\"name\":\"FromSoftware\"}}";
+		makePost(request, json, HttpStatus.SC_BAD_REQUEST);
+	}
+
+	@Test
+	public void insertGameWithInvalidPurchases_thenBadRequest() throws IOException {
+		HttpPost request = new HttpPost(SERVICE_URL);
+		String json = "{\"name\":\"ASDSD\",\"releaseDate\":\"2019-03-22\",\"price\":60,\"purchases\":-1300000,\"released\":true,\"publisher\":{\"name\":\"FromSoftware\"}}";
+		makePost(request, json, HttpStatus.SC_BAD_REQUEST);
+	}
+
+	@Test
+	public void insertGameWithInvalidPrice_thenBadRequest() throws IOException {
+		HttpPost request = new HttpPost(SERVICE_URL);
+		String json = "{\"name\":\"ASDSD\",\"releaseDate\":\"2019-03-22\",\"price\":360,\"purchases\":-1300000,\"released\":true,\"publisher\":{\"name\":\"FromSoftware\"}}";
 		makePost(request, json, HttpStatus.SC_BAD_REQUEST);
 	}
 
 	@Test
 	public void updateGame_thenOk() throws IOException {
 		HttpPut request = new HttpPut(SERVICE_URL);
-		String json = "{\"id\":1,\"name\":\"Sekiro\",\"releaseDate\":\"2019-03-22\",\"price\":60,\"purchases\":290000,\"released\":true,\"publisher\":{\"name\":\"FromSoftware\"}}";
+		String json = "{\"name\":\"Sekiro\",\"releaseDate\":\"2019-03-22\",\"price\":60,\"purchases\":290000,\"released\":true,\"publisher\":{\"name\":\"FromSoftware\"}}";
 		makePut(request, json, HttpStatus.SC_OK);
 	}
 
 	@Test
 	public void updateGame_thenNotFound() throws IOException {
 		HttpPut request = new HttpPut(SERVICE_URL);
-		String json = "{\"id\":9999,\"name\":\"NonExistentGame\",\"releaseDate\":\"2019-03-22\",\"price\":60,\"purchases\":130000,\"released\":true,\"publisher\":{\"name\":\"NonExistentPublisher\"}}";
+		String json = "{\"name\":\"NonExistentGame\",\"releaseDate\":\"2019-03-22\",\"price\":60,\"purchases\":130000,\"released\":true,\"publisher\":{\"name\":\"NonExistentPublisher\"}}";
 		makePut(request, json, HttpStatus.SC_NOT_FOUND);
+	}
+
+	@Test
+	public void updateGameWithInvalidPurchases_thenBadRequest() throws IOException {
+		HttpPut request = new HttpPut(SERVICE_URL);
+		String json = "{\"name\":\"Sekiro\",\"releaseDate\":\"2019-03-22\",\"price\":60,\"purchases\":-130000,\"released\":true,\"publisher\":{\"name\":\"NonExistentPublisher\"}}";
+		makePut(request, json, HttpStatus.SC_BAD_REQUEST);
+	}
+
+	@Test
+	public void updateGameWithInvalidPrice_thenBadRequest() throws IOException {
+		HttpPut request = new HttpPut(SERVICE_URL);
+		String json = "{\"name\":\"NonExistentGame\",\"releaseDate\":\"2019-03-22\",\"price\":360,\"purchases\":130000,\"released\":true,\"publisher\":{\"name\":\"NonExistentPublisher\"}}";
+		makePut(request, json, HttpStatus.SC_BAD_REQUEST);
 	}
 
 	@Test
 	public void deleteGame_thenOk() throws IOException {
 		HttpUriRequest request = new HttpDelete(STR."\{SERVICE_URL}/1");
-		addAuthorizationHeader(request);
+		addAuthorizationHeader(request, USERNAME_CLEANER);
 		HttpResponse httpResponse = HttpClientBuilder
 				.create()
 				.build()
@@ -257,7 +258,7 @@ public class JerseyApiTest {
 	@Test
 	public void deleteGame_thenNotFound() throws IOException {
 		HttpUriRequest request = new HttpDelete(STR."\{SERVICE_URL}/9999");
-		addAuthorizationHeader(request);
+		addAuthorizationHeader(request, USERNAME_CLEANER);
 		HttpResponse httpResponse = HttpClientBuilder
 				.create()
 				.build()
@@ -272,7 +273,7 @@ public class JerseyApiTest {
 	@Test
 	public void deleteAllGames_thenOk() throws IOException {
 		HttpUriRequest request = new HttpDelete(SERVICE_URL);
-		addAuthorizationHeader(request);
+		addAuthorizationHeader(request, USERNAME_CLEANER);
 		HttpResponse httpResponse = HttpClientBuilder
 				.create()
 				.build()
@@ -283,4 +284,6 @@ public class JerseyApiTest {
 				httpResponse.getStatusLine().getStatusCode()
 		);
 	}
+
+	// TODO: tests with failed auth
 }
