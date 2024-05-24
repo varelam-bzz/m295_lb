570025389d3ce6c620e5ad7336f2893507b95a9c
varelam-bzz <varelam@bzz.ch>
2024-05-24 11:50:17 +0200

    Implement max size for publisher name

--- src/main/java/ch/matias/m295_lb/models/Publisher.java
+++ src/main/java/ch/matias/m295_lb/models/Publisher.java
@@ -2,6 +2,7 @@ package ch.matias.m295_lb.models;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.List;
@@ -17,6 +18,7 @@ public class Publisher {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Size(max = 45, message = "Publisher name cannot exceed 45 characters.")
     private String name;
     @OneToMany(mappedBy = "publisher", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonIgnore
